d1f4c25c5bad88eaee4717bb77343727c5b07eac
VoidStillCodes <geraintwoodthorpe2008@gmail.com>
2026-01-06 14:19:22 +0000

    g

---
+++ screen.py
@@ -0,0 +1,10 @@
+#-- Imports --#
+import random
+#--------------#
+
+#-- Screen class --#
+class screen:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        
