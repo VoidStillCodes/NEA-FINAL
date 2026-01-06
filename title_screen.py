300ac868221aa889386b9eff838b2bcb712a5598
VoidStillCodes <geraintwoodthorpe2008@gmail.com>
2026-01-06 14:17:34 +0000

    title_screen

---
+++ title_screen.py
@@ -0,0 +1,20 @@
+#-- Imports --#
+import pygame
+from main import game
+#-------------#
+
+# TitleScreen class --#
+class TitleScreen:
+    def __init__(self, game):
+        self.game = game
+        self.menu_options = ["Start Game", "Load Game", "Options", "Exit"]
+        
+    
+    def handle_events(self):
+        pass 
+    
+    def update(self):
+        pass
+    
+    def render(self):
+        pass
